0502399eddf3f0db857751d31142e805c0497cfb
hebrerillo <hebrerillo@gmail.com>
2022-07-27 12:23:48 +0200

    Using prototypes instead of classes

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 'strict';
-
-class SnapApp
+(function ()
 {
-    #navigationMenu = document.querySelector('.navigation-menu'); //The nav item.
-    #rightMenu = document.querySelector('.right-menu-nav'); //The right menu.
-    #openRightMenuButton = document.querySelector('[data-open-right-menu]'); //The button to open the right menu.
-    #closeRightMenuButton = document.querySelector('[data-close-right-menu]'); //The button to close the right menu.
-    #menuLinks = document.querySelector('.menu-links'); //The container of the menu links in the header. It will be used to do event delegation.
-    #submenuTransitionEndCB;
-    constructor()
+    function SnapApp()
     {
-        this.#setInitialEvents();
+        this.navigationMenu = document.querySelector('.navigation-menu'); //The nav item.
+        this.rightMenu = document.querySelector('.right-menu-nav'); //The right menu.
+        this.openRightMenuButton = document.querySelector('[data-open-right-menu]'); //The button to open the right menu.
+        this.closeRightMenuButton = document.querySelector('[data-close-right-menu]'); //The button to close the right menu.
+        this.menuLinks = document.querySelector('.menu-links'); //The container of the menu links in the header. It will be used to do event delegation.
+        this.submenuTransitionEndCB = this.submenuTransitionEnd.bind(this);
+        this.setInitialEvents();
     }
 
     /**
      * Listen for some events.
      */
-    #setInitialEvents()
+    SnapApp.prototype.setInitialEvents = function ()
     {
-        this.#openRightMenuButton.addEventListener('click', this.#openRightMenu.bind(this));
-        this.#closeRightMenuButton.addEventListener('click', this.#closeRightMenu.bind(this));
-        this.#rightMenu.addEventListener('transitionend', this.#rightMenuOnTransitionEnd.bind(this));
-        this.#menuLinks.addEventListener('click', this.#menuItemClick.bind(this));
-        this.#submenuTransitionEndCB = this.#submenuTransitionEnd.bind(this);
-    }
+        this.openRightMenuButton.addEventListener('click', this.openRightMenu.bind(this));
+        this.closeRightMenuButton.addEventListener('click', this.closeRightMenu.bind(this));
+        this.rightMenu.addEventListener('transitionend', this.rightMenuOnTransitionEnd.bind(this));
+        this.menuLinks.addEventListener('click', this.menuItemClick.bind(this));
+        this.submenuTransitionEndCB = this.submenuTransitionEnd.bind(this);
+    };
 
     /**
      * Will be triggered when clicking on the whole container of links. Such a container has the class '.menu-links'.
      *
      * @param {Event} event The event triggered when performing the click. It will be used to do event delegation
      */
-    #menuItemClick(event)
+    SnapApp.prototype.menuItemClick = function (event)
     {
         let clickedElement = event.target.closest('.menu-item-title[data-opensecundary]');
         if (clickedElement)
@@ -45,7 +44,7 @@ class SnapApp
                 subMenu.offsetHeight;
                 subMenu.style.maxHeight = subMenu.scrollHeight + 'px';
 
-                subMenu.addEventListener('transitionend', this.#submenuTransitionEndCB);
+                subMenu.addEventListener('transitionend', this.submenuTransitionEndCB);
             }
             else
             {
@@ -54,52 +53,52 @@ class SnapApp
 
             return;
         }
-    }
+    };
 
     /**
      * Will be executed once the transition of the submenu ends. It will set to 'display:none' the submenu.
      * 
      * @param {Event} event The event triggered when the transition ends.
      */
-    #submenuTransitionEnd(event)
+    SnapApp.prototype.submenuTransitionEnd = function (event)
     {
         let subMenu = event.target;
         if (!subMenu.style.maxHeight)
         {
             subMenu.style.display = 'none';
-            subMenu.removeEventListener('transitionend', this.#submenuTransitionEndCB);
+            subMenu.removeEventListener('transitionend', this.submenuTransitionEndCB);
         }
-    }
+    };
 
     /**
      * Open the right menu. Only for mobile devices.
      */
-    #openRightMenu()
+    SnapApp.prototype.openRightMenu = function ()
     {
-        this.#navigationMenu.style.display = 'flex';
-        this.#rightMenu.offsetHeight; //Flush Css animation changes.
-        this.#rightMenu.classList.add('right-menu-nav--opened');
-    }
+        this.navigationMenu.style.display = 'flex';
+        this.rightMenu.offsetHeight; //Flush Css animation changes.
+        this.rightMenu.classList.add('right-menu-nav--opened');
+    };
 
     /**
      * Close the right menu. Only for mobile devices.
      */
-    #closeRightMenu()
+    SnapApp.prototype.closeRightMenu = function ()
     {
-        this.#rightMenu.classList.remove('right-menu-nav--opened');
-    }
+        this.rightMenu.classList.remove('right-menu-nav--opened');
+    };
 
     /**
      * This callback will be executed once the transition to hide the right menu ends. It will hide the whole navigation menu.
      */
-    #rightMenuOnTransitionEnd()
+    SnapApp.prototype.rightMenuOnTransitionEnd = function ()
     {
-        if (!this.#rightMenu.classList.contains('right-menu-nav--opened'))
+        if (!this.rightMenu.classList.contains('right-menu-nav--opened'))
         {
-            this.#navigationMenu.style.display = 'none';
+            this.navigationMenu.style.display = 'none';
         }
-    }
-}
+    };
 
-new SnapApp();
+    new SnapApp();
 
+})();
